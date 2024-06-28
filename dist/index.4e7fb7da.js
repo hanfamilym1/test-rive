0a032a0d28438e2fee30c581db853b4680394041
hanfamilym1 <michael@hanfamily.com>
2024-06-28 16:14:00 -0600

    Updating parcel and dist

---
+++ dist/index.4e7fb7da.js
@@ -0,0 +1,2 @@
+const t=window.innerWidth>0?window.innerWidth:screen.width;let e=document.getElementById("canvas");e.style.width=String(.8*t)+"px",e.style.height=String(1.3*t)+"px",e.style.left=String((t-.8*t)*.48)+"px",e.style.top="0px";
+//# sourceMappingURL=index.4e7fb7da.js.map
---
+++ dist/index.dc24e598.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",function(){setTimeout(function(){var e;(e=document.querySelector(".lds-ellipsis")).style.opacity=0,setTimeout(function(){e.style.display="none"},800)},3e3)});
+//# sourceMappingURL=index.dc24e598.js.map
